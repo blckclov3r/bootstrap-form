25ee0764e876c0ca163581552c9657371004a733
blckclov3r <blckclov3r@gmail.com>
2020-11-15 09:20:06 -0800

    commit

---
+++ main.js
@@ -0,0 +1,20 @@
+document.getElementById("firstname").maxLength = 10;
+
+function isNumber(evt){
+    evt = evt ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
+
+
+function isNumericKey(evt)
+{
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode != 46 && charCode > 31 
+    && (charCode < 48 || charCode > 57))
+    return true;
+    return false;
+} 
